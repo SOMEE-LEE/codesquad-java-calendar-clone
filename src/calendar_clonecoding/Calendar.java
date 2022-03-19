4e61c978bec3b2d675578c585b0dcfdc38607a54
2005950 <dream970307!>
2022-03-19 19:50:31 +0900

    Add sum of two numbers

--- src/calendar_clonecoding/Calendar.java
+++ src/calendar_clonecoding/Calendar.java
@@ -1,11 +1,19 @@
 package calendar_clonecoding;
 
+import java.util.Scanner;
+
 public class Calendar {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello, Calendar");
 
+		// 키보드로 두 정수의 입력을 받는다.
+		System.out.println("2개의 정수를 입력하세요: ");
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+
+		// 두 정수를 입력하면 화면에 두 정수의 합을 출력한다.
+		System.out.println("두 수의 합은" + (a + b) + "이다");
+		// sc.close();
 	}
 
 }
