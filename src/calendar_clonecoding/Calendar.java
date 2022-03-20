79774eb6fdc75410f9209c66284e1640657d028f
2005950 <dream970307!>
2022-03-20 17:19:03 +0900

    Find max days of the month ("array" version -> "if" version is wrong)

--- src/calendar_clonecoding/Calendar.java
+++ src/calendar_clonecoding/Calendar.java
@@ -9,17 +9,10 @@ public class Calendar {
 		System.out.println("최대 일수를 알고 싶은 달을 입력하세요: ");
 		Scanner sc = new Scanner(System.in);
 		int month = sc.nextInt();
-		int days[] = { 29, 30, 31 };
+		int[] maxDays = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
 		// 입력받은 달의 최대 일수를 화면에 출력한다.
-		if (month == 2) {
-			System.out.printf("2월의 최대 일수는 %d일이다.", days[0]);
-		} else if (month > 2 && month % 2 == 0) {
-			System.out.printf("%d월의 최대 일수는 %d일이다.", month, days[1]);
-		} else if (month % 2 == 1) {
-			System.out.printf("%d월의 최대 일수는 %d일이다.", month, days[2]);
-		}
-
+		System.out.printf("%d월은 %d일까지 있습니다.\n", month, maxDays[month - 1]);
 		sc.close();
 	}
 
