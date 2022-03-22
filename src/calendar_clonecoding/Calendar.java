ac08cdefbc16280d4684da99aace2301222af306
2005950 <dream970307!>
2022-03-22 22:29:58 +0900

    can repeat input (for)

--- src/calendar_clonecoding/Calendar.java
+++ src/calendar_clonecoding/Calendar.java
@@ -3,39 +3,52 @@ package calendar_clonecoding;
 import java.util.Scanner;
 
 public class Calendar {
+	// 1월~12월의 최대 일수들을 순서대로 배열 안에 넣음
+	public static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+	// MAX_DAYS 정적 메소드를 만들고 1월~12월의 최대 일수들을 순서대로 배열 안에 넣음
+	public int getMaxDaysOfMonth(int month) {
+		return MAX_DAYS[month - 1];
+	}
+
+// 가상 달력 샘플 (void 메소드인 printSampleCalendar를 만들어 가상의 달력 생성)
+	public void printSampleCalendar() {
+		System.out.println("  일 월  화  수  목 금  토");
+		System.out.println("----------------------");
+		System.out.println("  1  2  3  4  5  6  7");
+		System.out.println("  8  9 10 11 12 13 14");
+		System.out.println(" 15 16 17 18 19 20 21");
+		System.out.println(" 22 23 24 25 26 27 28");
+	}
+
 	public static void main(String[] args) {
+		// 숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
+		// 스캐너 열기
 		Scanner sc = new Scanner(System.in);
-		// 입력받은 달의 최대 일수를 화면에 출력한다.
-		System.out.println("최대 일수를 알고 싶은 월을 입력하세요: ");
-		// 입력받을 달을 month라는 변수로 설정
-		int month = sc.nextInt();
-		// 월별 최대 일수를 배열에 저장
-		int[] maxDays = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-
-		// 0 이하의 숫자나 12보다 큰 숫자 입력 시 출력할 문구
-		if (month <= 0 || month > 12) {
-			System.out.printf("\n%d월은 없습니다.", month);
-
-			// 정상적으로 1~12사이의 숫자를 입력하면 출력할 문구
-		} else {
-			System.out.println("반복횟수를 입력하세요: ");
-		}
-
-		// n번 반복 출력이 가능하게 한다.
-		// 입력받을 반복 횟수를 repeat이라는 변수로 설정
+		// 자바의 Calendar 클래스를 cal이라는 이름으로 생성
+		Calendar cal = new Calendar();
+		// 반복횟수를 입력하라는 문구를 출력하고, repeat이라는 변수로 숫자를 입력받음
+		System.out.println("반복횟수를 입력하세요.");
 		int repeat = sc.nextInt();
-		// 0이하의 숫자 입력 시 출력할 문구
-		if (repeat <= 0) {
-			System.out.printf("\n%d번 반복은 할 수 없습니다.", repeat);
+
+		// 정수 i를 0으로 설정하고, repeat에 입력한 수보다 작으면 1씩 증가시킨다.
+		for (int i = 0; i < repeat; i++) {
+			// 달을 입력하라는 문구를 출력하고, month라는 변수로 숫자를 입력받음
+			System.out.println("달을 입력하세요");
+			int month = sc.nextInt();
+			// 입력한 월에 대한 최대 일수를 출력
+			System.out.printf("%d월은 %d일까지 있습니다. \n", month, cal.getMaxDaysOfMonth(month));
 		}
 
-		// 문장 반복 출력을 위해 새로 변수를 만들고 값을 1로 설정
-		int num = 1;
-		// 위의 넘 변수++로 repeat에 입력한 반복횟수만큼 아래 문장을 출력함
-		while (repeat != 0 && repeat >= num) {
-			System.out.printf("%d월은 %d일까지 있습니다.\n", month, maxDays[month - 1]);
-			num++;
-		}
+		// "Bye~" 문구 출력
+		System.out.println("Bye~");
+		/*
+		 * 메인 메소드에서 Calendar 클래스인 cal 뒤에, 위의 void 메소드인 printSampleCalendar를 호출하여 가상 달력
+		 * 샘플 출력
+		 */
+		cal.printSampleCalendar();
+		// 스캐너 닫기
 		sc.close();
 	}
+
 }
