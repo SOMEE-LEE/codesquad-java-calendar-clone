252466410efd14d5db46d908ace0fc0140ddd1c8
2005950 <dream970307!>
2022-03-26 22:32:10 +0900

    create simple UI

--- src/calendar_clonecoding/Prompt.java
+++ src/calendar_clonecoding/Prompt.java
@@ -3,8 +3,46 @@ package calendar_clonecoding;
 import java.util.Scanner;
 
 public class Prompt {
+
 	int month = 0;
 	int year = 0;
+
+	public static void runCommand() {
+		// 스캐너 열기
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("+-----------------+");
+		System.out.println("| 1. 일정 등록");
+		System.out.println("| 2. 일정 검색");
+		System.out.println("| 3. 일정 달력 보기");
+		System.out.println("| h. 도움말  q. 종료");
+		System.out.println("+-----------------+");
+
+		System.out.println("명령을 입력하세요 (1, 2, 3, h, q)");
+		System.out.print("COMMAND> ");
+		String command = sc.nextLine();
+
+		Prompt p = new Prompt();
+
+		if (command.equals("1")) {
+			System.out.println("일정 등록");
+		} else if (command.equals("2")) {
+			System.out.println("일정 검색");
+		} else if (command.equals("3")) {
+			p.runPrompt();
+		} else if (command.equals("h")) {
+			System.out.println("도움말");
+		} else if (command.equals("q")) {
+			System.out.println("\nBye~");
+			// 스캐너 닫기
+			sc.close();
+		} else {
+			System.out.println("잘못된 입력입니다.");
+			System.out.println("\n명령을 입력하세요 (1, 2, 3, h, q)");
+			System.out.print("COMMAND> ");
+			command = sc.nextLine();
+		}
+	}
 
 	public void runPrompt() {
 		// 스캐너 열기
@@ -15,7 +53,7 @@ public class Prompt {
 		// 횟수가 사라지지 않는 반복 - while문에 true를 사용하여 무한루프
 		while (true) {
 			// 년도를 입력하라는 문구와 프롬프트 문자 및 커서 출력, year라는 변수로 년도에 해당하는 숫자를 입력받음
-			System.out.println("년도를 입력하세요. (exit: -1)");
+			System.out.println("\n년도를 입력하세요. (exit: -1)");
 			System.out.print("YEAR> ");
 			year = sc.nextInt();
 			if (year == -1)
@@ -35,16 +73,14 @@ public class Prompt {
 			cal.printCalendar(year, month);
 		}
 
-		// "Bye~" 문구 출력
 		System.out.println("Bye~");
 		// 스캐너 닫기
 		sc.close();
 	}
 
 	public static void main(String[] args) {
-		// 셀 실행
-		Prompt p = new Prompt();
-		p.runPrompt();
+		// 프롬프트 클래스 실행
+		runCommand();
 	}
 
 }
